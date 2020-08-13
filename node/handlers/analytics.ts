10effb86b9475dfc39ce37823024901edccd6da4
Sinésio Neto <sn.sineto@gmail.com>
2020-08-13 17:37:21 -0300

    update repository

--- node/handlers/analytics.ts
+++ node/handlers/analytics.ts
@@ -1,7 +1,9 @@
 export async function analytics(ctx: Context, next: () => Promise<any>) {
   if (ctx.method.toUpperCase() === 'GET') {
+    const { clients: { analytics } } = ctx
     ctx.status = 200
-    ctx.body = 'OK'
+    // ctx.body = 'OK'
+    ctx.body = await analytics.getLiveUsers()
 
     ctx.set('cache-control', 'no-cache')
   }
